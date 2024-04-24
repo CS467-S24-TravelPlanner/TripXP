12a37c114d8fcabeeaf330f4495db24943c6f8a9
hrummel1 <123182419+hrummel1@users.noreply.github.com>
2024-04-24 11:44:37 -0500

    set up routing from app.jsx to profile page

---
+++ frontend/src/components/ProfilePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const ProfilePage = () => {
+    return (
+        <div>
+            <h1>Profile Page Loaded!</h1>
+        </div>
+    )
+}
+
+export default ProfilePage;
