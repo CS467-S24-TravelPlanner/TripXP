ab646d1673de698634c3254b07aaa4bf88cba1c9
ChaseSmith67 <smitcha6@oregonstate.edu>
2024-04-26 10:24:24 -0500

    add a few comments

--- frontend/src/pages/ExperienceSearch.jsx
+++ frontend/src/pages/ExperienceSearch.jsx
@@ -8,30 +8,41 @@ function ExperienceSearch() {
   const [searchParams, setSearchParams] = useState("NONE");
 
   useEffect(() => {
+    // Get all experiences from the DB
     getExperiences().then((results) => {
       if (searchParams) {
+        // Create empty list of experiences
         let updatedExpList = { data: [] };
+        // Iterate through experiences
         for (let i = 0; i < results.data.length; i++) {
           let exp = results.data[i];
+          // Split experience keywords into an array - easier to work with.
           let expKeywords = exp.keywords.toString().split(",");
+          // Iterate through experience keywords
           for (let j = 0; j < expKeywords.length; j++) {
+            // If matching keyword is found, add that experience to the list
             if (expKeywords[j] == searchParams) {
               updatedExpList.data.push(exp);
+              break; // Experience added to list, no need to keep checking keywords
             }
           }
         }
+        // Return list of experiences with matching keywords
         setExpList(updatedExpList);
       } else {
+        // Return all experiences if no search parameters
         setExpList(results);
       }
     });
   }, [searchParams]);
 
+  // Updates search parameters when submitted - triggers useEffect hook
   const handleSubmit = (e) => {
     setSearchParams(searchInput);
     e.preventDefault();
   };
 
+  // Updates input as it is changed
   const handleChange = (e) => {
     setSearchInput(e.target.value);
     e.preventDefault();
