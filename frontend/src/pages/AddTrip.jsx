5c7e4de6b3709bb7b6f3ef0d6704891bf0feb015
alclary <alclary@posteo.net>
2024-05-20 23:28:50 -0400

    toast component built for user feedback; see AddTrip page for example of implementation

--- frontend/src/pages/AddTrip.jsx
+++ frontend/src/pages/AddTrip.jsx
@@ -2,11 +2,17 @@ import React, { useState, useEffect } from "react";
 import { createTrip } from "../utilities/TripHandler";
 import { useNavigate } from "react-router-dom";
 import TripDetails from "../components/TripDetails";
+import Toast from "../components/Toast";
 
 const AddTrip = () => {
   // TODO user id (or better unique identifier) pulled from client, pending auth imp
   const [trip, setTrip] = useState({ name: "", description: "", user_id: 2 });
   const navigate = useNavigate();
+  const [toast, setToast] = useState({
+    show: false,
+    severity: "",
+    message: "",
+  });
 
   const handleChange = (e) => {
     // TODO data validation
@@ -24,7 +30,8 @@ const AddTrip = () => {
         console.log(`Trip ${res.id} created successfully.`);
         navigate(`/trip/edit/${res.id}`);
       } else {
-        console.error("Error creating trip:", res.error);
+        setToast({ show: true, severity: "error", message: `${res.message}` });
+        console.error("Error creating trip:", res.message);
       }
     } catch (error) {
       console.error("Create trip failed:", error);
@@ -38,6 +45,11 @@ const AddTrip = () => {
         trip={trip}
         handleChange={handleChange}
         handleSubmit={handleSubmit}
+      />
+      <Toast
+        show={toast.show}
+        severity={toast.severity}
+        message={toast.message}
       />
     </>
   );
