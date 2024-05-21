295bc30cd4875d76beb4a9962eae3e061690775a
ChaseSmith67 <smitcha6@oregonstate.edu>
2024-05-21 02:26:40 -0500

    modify upload path

--- backend/src/app.js
+++ backend/src/app.js
@@ -229,7 +229,7 @@ sequelize
 
     let storage = multer.diskStorage({
       destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, '/uploads/'));
+        cb(null, '/uploads/');
       },
       filename: function (req, file, cb) {
         cb(null, new Date().toISOString() +  file.originalname);
