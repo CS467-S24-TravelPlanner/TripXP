58927b88d37201e49cba2993e8e25ae1d76be7f9
ChaseSmith67 <smitcha6@oregonstate.edu>
2024-05-21 02:38:56 -0500

    more changes to routing

--- backend/src/app.js
+++ backend/src/app.js
@@ -266,7 +266,7 @@ sequelize
     }
 
     let fileName = req.query.fileName
-    res.sendFile(fileName, options, function (err) {
+    res.sendFile('/uploads/' + fileName, options, function (err) {
       if (err) {
         next(err)
       } else {
