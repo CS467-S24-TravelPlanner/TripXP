aee56b1f1872a5092e0c7b48a294ed64c4b3c2f3
alclary <alclary@posteo.net>
2024-05-21 19:39:36 -0400

    trip handler updated for new API changes and auth

--- frontend/src/utilities/TripHandler.jsx
+++ frontend/src/utilities/TripHandler.jsx
@@ -1,21 +1,16 @@
 import { postData, getData, patchData, deleteData } from "./ApiService.jsx";
-import { getExperiences } from "./ExperienceHandler.jsx";
 
 /**
  * createTrip adds a new Trip to the Database.
  * The return value is the response from the server in JSON format.
  * @param {string} name - The name of the new Trip.
  * @param {string} description - The description of the new Trip.
- * @param {number} user_id - An ID of the User creating the new Trip.
+ * @param {string} auth_token - The full JWT for authorization.
  */
-function createTrip(name, description, user_id) {
-  const newTrip = {
-    name: name,
-    description: description,
-    user_id: user_id,
-  };
+function createTrip(name, description, auth_token) {
+  const newTrip = { name, description };
 
-  return postData("/trip", {}, newTrip);
+  return postData("/trip", {}, newTrip, auth_token);
 }
 
 /**
@@ -23,36 +18,49 @@ function createTrip(name, description, user_id) {
  * The return value is the response from the server in JSON format.
  * @param {number} tripId - The ID of the Trip.
  * @param {number} experienceId - An ID of the Experience being added to the Trip.
+ * @param {string} auth_token - The full JWT for authorization.
  */
-function addExperienceToTrip(tripId, experienceId) {
-  return postData("/trip/" + tripId + "/experience/" + experienceId, {});
+function addExperienceToTrip(tripId, experienceId, auth_token) {
+  return postData(
+    "/trip/" + tripId + "/experience/" + experienceId,
+    {},
+    {},
+    auth_token
+  );
 }
 
 /**
  * getTrip returns the trip matching the given ID from the Database.
  * The return value is the response from the server in JSON format.
  * @param {number} tripId - The ID of the Trip.
+ * @param {string} auth_token - The full JWT for authorization.
  */
-function getTrip(tripId) {
-  return getData("/trip/" + tripId);
+function getTrip(tripId, auth_token) {
+  return getData("/trip/" + tripId, {}, auth_token);
 }
 
 /**
  * getTrips returns Trips matching the search parameters from the Database.
  * The return value is the response from the server in JSON format.
  * @param {JSON} searchParams - The optional search parameters for desired Trip(s).
+ * @param {string} auth_token - The full JWT for authorization.
  */
-function getTrips(searchParams = {}) {
-  return getData("/trip", searchParams);
+function getTrips(searchParams = {}, auth_token) {
+  return getData("/trip", searchParams, auth_token);
 }
 
 /**
  * getTripExperiences returns Experiences belonging to a Trip from the Database.
  * The return value is an Array of Experience objects.
  * @param {number} tripId - The ID of the Trip whose Experiences will be returned.
+ * @param {string} auth_token - The full JWT for authorization.
  */
-async function getTripExperiences(tripId) {
-  let experiences = await getData("/trip/" + tripId + "/experience");
+async function getTripExperiences(tripId, auth_token) {
+  let experiences = await getData(
+    "/trip/" + tripId + "/experience",
+    {},
+    auth_token
+  );
   return experiences;
 }
 
@@ -62,25 +70,27 @@ async function getTripExperiences(tripId) {
  * The return value is the response from the server in JSON format.
  * @param {number} id - The ID of the Trip being modified.
  * @param {JSON} newData - The key/value pairs being modified.
+ * @param {string} auth_token - The full JWT for authorization.
  */
-function editTrip(id, newData = {}) {
-  const idObj = { id: id };
-  const oldTrip = getTrips(idObj);
+function editTrip(id, newData = {}, auth_token) {
+  const oldTrip = getTrip(id, auth_token);
 
+  // TODO more security likely needed here to ensure id can't be overwritten
   const newTrip = Object.keys(newData).length
-    ? { ...idObj, ...newData }
+    ? { ...oldTrip, ...newData }
     : oldTrip;
 
-  return patchData("/trip", newTrip);
+  return patchData("/trip", newTrip, auth_token);
 }
 
 /**
  * deleteTrip removes an existing Trip in the Database.
  * The return value is the response from the server in JSON format.
  * @param {number} id - The ID of the Trip being deleted.
+ * @param {string} auth_token - The full JWT for authorization.
  */
-function deleteTrip(id) {
-  return deleteData("/trip", { id: id });
+function deleteTrip(id, auth_token) {
+  return deleteData("/trip", { id }, auth_token);
 }
 
 /**
@@ -88,9 +98,14 @@ function deleteTrip(id) {
  * The return value is the response from the server in JSON format.
  * @param {number} tripId - The ID of the Trip.
  * @param {number} experienceId - An ID of the Experience being removed from the Trip.
+ * @param {string} auth_token - The full JWT for authorization.
  */
-function removeExperienceFromTrip(tripId, experienceId) {
-  return deleteData("/trip/" + tripId + "/experience/" + experienceId, {});
+function removeExperienceFromTrip(tripId, experienceId, auth_token) {
+  return deleteData(
+    "/trip/" + tripId + "/experience/" + experienceId,
+    {},
+    auth_token
+  );
 }
 
 export {
