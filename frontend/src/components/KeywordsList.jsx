807114da947078fd3ab14011553f2f87e50a7ef3
alclary <alclary@posteo.net>
2024-05-24 23:15:44 -0400

    correct keyword label overlap for keyword multi-select

--- frontend/src/components/KeywordsList.jsx
+++ frontend/src/components/KeywordsList.jsx
@@ -1,51 +1,61 @@
 import { getKeywords } from "../utilities/Keywords";
 import { React } from "react";
-import { InputLabel, MenuItem, ListItemText, Select, OutlinedInput, Checkbox } from "@mui/material";
-
+import {
+  MenuItem,
+  FormControl,
+  TextField,
+  ListItemText,
+  OutlinedInput,
+  Checkbox,
+} from "@mui/material";
 
 const keywordsList = getKeywords();
 
-function KeywordsList({ keywords, setKeywords }) { 
-
-    const ITEM_HEIGHT = 48;
-    const ITEM_PADDING_TOP = 8;
-    const MenuProps = {
-      PaperProps: {
-        style: {
-          maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-          width: 250,
-        },
+function KeywordsList({ keywords, setKeywords }) {
+  const ITEM_HEIGHT = 48;
+  const ITEM_PADDING_TOP = 8;
+  const MenuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+        width: 250,
       },
-    };
+    },
+  };
 
-    const handleKeywordsChange = (e) => {
-        let newList = typeof e.target.value === "string" ? value.split(",") : e.target.value;
-        setKeywords(newList)
-      };
+  const handleKeywordsChange = (e) => {
+    let newList =
+      typeof e.target.value === "string" ? value.split(",") : e.target.value;
+    setKeywords(newList);
+  };
 
-    return (
-        <div>
-        <InputLabel id="keywords-label">Keywords</InputLabel>
-          <Select
-            labelId="keywords-label"
-            id="keywordsSelect"
-            multiple
-            fullWidth
-            value={keywords}
-            onChange={handleKeywordsChange}
-            input={<OutlinedInput />}
-            renderValue={(selected) => selected.join(", ")}
-            MenuProps={MenuProps}
-          >
-            {keywordsList.map((keyword) => (
-              <MenuItem key={keyword} value={keyword}>
-                <Checkbox checked={keywords.indexOf(keyword) > -1} />
-                <ListItemText primary={keyword} />
-              </MenuItem>
-            ))}
-          </Select>
-          </div>
-    )
-};
+  return (
+    <div>
+      <FormControl fullWidth>
+        <TextField
+          id="keywordsSelect"
+          label="Keywords"
+          select
+          SelectProps={{
+            multiple: true,
+            value: keywords,
+            onChange: handleKeywordsChange,
+            input: <OutlinedInput label="Keywords" />,
+            renderValue: (selected) => selected.join(", "),
+            MenuProps: MenuProps,
+          }}
+          variant="outlined"
+        >
+          {keywordsList.map((keyword) => (
+            <MenuItem key={keyword} value={keyword}>
+              <Checkbox checked={keywords.indexOf(keyword) > -1} />
+              <ListItemText primary={keyword} />
+            </MenuItem>
+          ))}
+        </TextField>
+      </FormControl>
+    </div>
+  );
+}
 
 export default KeywordsList;
